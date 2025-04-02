f56a589809b6667b253930bd4cefe4d02e04d3ea
seifeddinesaad <saadseif1925@outlook.com>
2025-04-02 17:37:07 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -10,16 +9,14 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@supabase/supabase-js": path.resolve(
-        __dirname,
-        "node_modules/@supabase/supabase-js"
-      ),
     },
   },
 }));
